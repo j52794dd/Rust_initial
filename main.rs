fecc3919db220b5ee306c4629c6537a7241fc602
David Demetriad <73079562+j52794dd@users.noreply.github.com>
2024-11-11 05:32:11 +0000

    Lifetimes

--- main.rs
+++ main.rs
@@ -1,17 +1,99 @@
+use std::any::type_name;
+use std::ops::Deref;
+use std::fmt;
+use std::fmt::Formatter;
+
+struct Shuttle<'a>{
+    name: &'a str,
+}
+
+impl<'a, 'b> Shuttle<'a> {
+    fn send_transmission(&self, msg: &str) -> &str{
+        println!("Transmission message: {}", msg);
+        self.name
+    }
+    fn send_message(&self, msg: &'b str) -> &'b str{
+        msg
+    }
+}
+
 fn main() {
-    let mut a: f32 = 0.671985;
-    let mut b = 0b1111_0000u8;
-    let mut c: u16 = 0x23f9;
-
-    println!("a is {2:<>9.6}, b is {0:016b}, c is {1:08o}", b, c, a);
-
     println!("Hello, world!");
+    let result;
+    let x = String::from("hello");
+    {
+        let mut y = x.clone();
+        y.push_str(", world!");
+        result = longer_str(&x, &y);
+    }
+    println!("result: {}", result);
+    let mut vehicle: Shuttle;
+    {
+        let comm = String::from("fcsb e steaua");
+        vehicle = Shuttle{name: "234"};
+    }
+    let sender = vehicle.send_transmission("Greetings! ");
+    println!("sender: {}", sender);
 
-    let mut a = 13;
-    let b = 2.3;
-    let c: f32 = 120.0;
+    let my_number = 8u8;
+    match my_number {
+        0 | 5 => {println!("zero"); println!("zero")},
+        1 => {println!("one"); println!("one")},
+        2 => {println!("two"); println!("two")},
+        3..9 => {println!("three"); println!("three")},
+        default=> {println!("default={}", default)},
+    }
 
-    let average = ((a as f64) + (b as f64) + (c as f64)) / 3.0;
-    assert_eq!(average, 45.1);
-    println!("Test passed!");
+    let countdown = [5,4,3,2,1];
+    let Some(&number) = countdown.get(4) else { todo!() };
+    //let number = *number + 1;
+    //println!("number is of type : {}",  type_of(*(number.unwrap())));
+    println!("number: {}", number);
+
+
+    let address = Location::Unknown;
+    //address.display();
+    println!("{}", address);
+    let address = Location::Anonymous;
+    //address.display();
+    println!("{}", address);
+    let address = Location::Known(28.608295, -80.604177);
+    //address.display();
+    println!("{}", address);
+
+
+}
+
+impl fmt::Display for Location {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        match self {
+            Location::Unknown => write!(f, "Unknown Location"),
+            Location::Anonymous => write!(f, "Anonymous Location"),
+            Location::Known(x, y) => write!(f, "Known Location (latitude and longitude) : ({}, {})", x, y),
+        }
+    }
+}
+
+enum Location{
+    Unknown,
+    Anonymous,
+    Known(f64, f64),
+}
+
+impl Location{
+    fn display(&self){
+        match self {
+            Location::Unknown => println!("Unknown Location"),
+            Location::Anonymous => println!("Anonymous Location"),
+            Location::Known(x, y) => println!("Known Location (latitude and longitude) : ({}, {})", x, y),
+        };
+    }
+}
+
+fn type_of<T>(_: T) -> &'static str {
+    type_name::<T>()
+}
+
+fn longer_str<'a, 'b>(x: &'a str, y: &'b str) -> &'a str {
+    x
 }
