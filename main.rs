7b904a4563f30d2aeeaed799da385c9c8cb766fb
David Demetriad <73079562+j52794dd@users.noreply.github.com>
2024-11-11 05:28:12 +0000

    Read-write

--- main.rs
+++ main.rs
@@ -1,17 +1,27 @@
+use std::env;
+use std::fmt::format;
+use std::fs;
+use std::io::{ Write};
+
 fn main() {
-    let mut a: f32 = 0.671985;
-    let mut b = 0b1111_0000u8;
-    let mut c: u16 = 0x23f9;
+    if env::args().len() < 3 {
+        println!("Number of arguments provided not enough");
+        return;
+    }
+    let args: Vec<String> = env::args().collect();
+    let mut path = args[1].clone();
+    let mut name = args[2].clone();
+    println!("File Path is: {:?}", path);
+    for (index, line) in fs::read_to_string(&path).expect(&format!("Could not read {}", path)).lines().enumerate() {
+        if line.eq(name.as_str()) {
+            fs::write("result.txt", &format!("Name found at position {}", index)).expect(&format!("Could not write {}", "result.txt"));
+            return;
+       }
+    }
+    fs::write("result.txt", &format!("{} not found, added to the end of the text file", name)).expect(&format!("Could not write {}", name));
+    let mut file_handle = fs::OpenOptions::new().append(true).open(path).expect("Could not open file");
+    file_handle.write(b"\n");
+    file_handle.write(name.as_bytes()).expect("Could not write to file");
 
-    println!("a is {2:<>9.6}, b is {0:016b}, c is {1:08o}", b, c, a);
-
-    println!("Hello, world!");
-
-    let mut a = 13;
-    let b = 2.3;
-    let c: f32 = 120.0;
-
-    let average = ((a as f64) + (b as f64) + (c as f64)) / 3.0;
-    assert_eq!(average, 45.1);
-    println!("Test passed!");
 }
+
